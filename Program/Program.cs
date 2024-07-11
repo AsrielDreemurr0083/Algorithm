31a45b8883bb52a1c82a21525f3b27b4c91bae66
AsrielDreemurr0083 <103588221+AsrielDreemurr0083@users.noreply.github.com>
2024-07-11 15:44:45 +0900

    (2024.7.11) 시간 복잡도

---
+++ Program/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace Program
+{
+    internal class Program
+    {
+        static bool IsPrime(int n)
+        {
+            for(int i = 2; i < n; i++)
+            {
+                if(n % i == 0)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+        static void Main(string[] args)
+        {
+            //합성수 : 1보다 큰 자연수 중에서 소수가 아닌 수로, 약수가 3개 이상인 수
+
+            //int n = int.Parse(Console.ReadLine());
+            //
+            //Console.WriteLine(IsPrime(n));
+
+            #region 에라토스테네스의 체
+            //소수를 판정하는 방법으로, 자연수를 순서대로
+            //늘어놓은 표에서 합성수를 차례대로 지워나가면서
+            //소수의 목록을 얻어내는 방법이다.
+
+
+
+            #endregion
+
+
+        }
+    }
+}
