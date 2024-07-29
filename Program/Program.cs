7590ea48ca75ca708fb8022b2f6a1941d452cd4c
AsrielDreemurr0083 <103588221+AsrielDreemurr0083@users.noreply.github.com>
2024-07-29 15:36:24 +0900

    (2024.7.29) 유니온 파인드

--- Program/Program.cs
+++ Program/Program.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
 //시험 범위 포함
 namespace Program
 {
@@ -7,7 +9,7 @@ namespace Program
     {
         static void Main(string[] args)
         {
-
+            
         }
     }
 }
