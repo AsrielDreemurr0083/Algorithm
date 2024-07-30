50dda490d309450dd7f170b8a1bf196e5ba84900
AsrielDreemurr0083 <103588221+AsrielDreemurr0083@users.noreply.github.com>
2024-07-30 15:40:51 +0900

    (2024.7.30) 최소 신장 트리

--- Program/Program.cs
+++ Program/Program.cs
@@ -2,14 +2,13 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Drawing;
-//시험 범위 포함
 namespace Program
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            
+
         }
     }
 }
